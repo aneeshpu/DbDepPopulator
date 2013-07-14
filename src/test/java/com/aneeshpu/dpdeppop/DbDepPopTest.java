72cb311a630e74a473c37203d9ed8fcffd1f9e16
Aneesh P U <aneeshp@thoughtworks.com>
2013-07-14 13:52:08 +0530

    Initial add

---
+++ src/test/java/com/aneeshpu/dpdeppop/DbDepPopTest.java
@@ -0,0 +1,45 @@
+package com.aneeshpu.dpdeppop;
+
+import org.junit.Test;
+
+import java.sql.*;
+
+public class DbDepPopTest {
+
+    @Test
+    public void gets_metadata() throws SQLException {
+
+        final Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/datadeppop", "datapopulator", "letmein");
+        final DatabaseMetaData metaData = connection.getMetaData();
+        final ResultSet paymentTableResultSet = metaData.getColumns(null, null, "payment", null);
+
+        final ResultSetMetaData paymentTableResultSetMetaData = paymentTableResultSet.getMetaData();
+        System.out.println(paymentTableResultSetMetaData.getColumnCount());
+
+        while (paymentTableResultSet.next()) {
+            for (int i = 1; i <= paymentTableResultSetMetaData.getColumnCount(); i++) {
+
+//                System.out.print(paymentTableResultSet.getString(i) + "\t");
+            }
+        }
+
+        final ResultSet paymentCrossReference = metaData.getImportedKeys(null, null, "payment");
+
+//        final ResultSet paymentCrossReference = metaData.getCrossReference(null, null, null, null, null, "payment");
+        paymentCrossReference.next();
+        System.out.print(paymentCrossReference.getString("pktable_name") + "\t");
+        System.out.print(paymentCrossReference.getString("pkcolumn_name") + "\t");
+        System.out.print(paymentCrossReference.getString("fktable_name") + "\t");
+        System.out.print(paymentCrossReference.getString("fkcolumn_name") + "\t");
+
+        for (int i = 1; i <= paymentCrossReference.getMetaData().getColumnCount(); i++) {
+//                System.out.print(paymentCrossReference.getString(i) + "\t");
+
+            while (paymentCrossReference.next()) {
+
+
+                System.out.println("\n");
+            }
+        }
+    }
+}

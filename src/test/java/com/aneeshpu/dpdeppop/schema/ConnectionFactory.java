a264d5c410f1e04c5b3723b5d897ff07b20a1ca6
Aneesh P U <aneeshp@thoughtworks.com>
2013-09-08 16:35:14 +0530

    Improved logging in ConnectionFactory

--- src/test/java/com/aneeshpu/dpdeppop/schema/ConnectionFactory.java
+++ src/test/java/com/aneeshpu/dpdeppop/schema/ConnectionFactory.java
@@ -10,13 +10,13 @@ public class ConnectionFactory {
     private static final Logger LOG = Logger.getLogger(ConnectionFactory.class);
 
     public Connection invoke() {
+        final String url = "jdbc:postgresql://localhost/datapopulator_test";
+        final String username = "datapopulator";
         try {
-            return DriverManager.getConnection("jdbc:postgresql://localhost/datapopulator_test", "datapopulator", "letmein");
+            return DriverManager.getConnection(url, username, "letmein");
         } catch (SQLException e) {
-            final String url = "jdbc:postgresql://localhost/datadeppop";
-            final String username = "datapopulator";
+            LOG.error("could not connect to " + url + " as " + username, e);
 
-            LOG.error("could not connect to " + url + " as " + username);
             throw new CouldNotConnectException(url, username);
         }
     }
